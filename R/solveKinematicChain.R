258be61714929ff099b38b9ce65ea5caf6e84228
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-01-20 13:50:16 -0500

    Added functionality to solve 'SRPSS' chain

--- R/solveKinematicChain.R
+++ R/solveKinematicChain.R
@@ -1,11 +1,11 @@
 solveKinematicChain <- function(joint.types = NULL, joints.unknown = NULL, joint.coor = NULL, 
-	joint.cons = NULL, joints.dist = NULL, joints.prev = NULL,
+	joint.cons = NULL, joints.dist = NULL, joints.prev = NULL, joint.init = NULL, 
 	query = NULL, reverse = FALSE, print.progress = FALSE){
 
 	if(!is.null(joint.coor) && is.null(joints.unknown)) stop("If 'joint.coor' is non-NULL, 'joints.unknown' must also be non-NULL.")
 
 	# ALLOWED CHAINS
-	type_str_ok <- type_str_rev_ok <- c('RrSpS', 'LtSpS', 'SSpPpSpS')
+	type_str_ok <- type_str_rev_ok <- c('RrSpS', 'LtSpS', 'SSpPpSpS', 'SRrPpSpS')
 
 	# ADD REVERSE ORDER CHAINS
 	for(i in 1:length(type_str_ok)) type_str_rev_ok <- c(type_str_rev_ok, paste(rev(strsplit(type_str_ok[i], split="")[[1]]), collapse=''))
@@ -16,7 +16,7 @@ solveKinematicChain <- function(joint.types = NULL, joints.unknown = NULL, joint
 	
 	# RETURN QUERY
 	if(!is.null(query)){
-		if(query == 'max path length') return(5)
+		if(query == 'max path length') return(max(nchar(gsub('[a-z]','',type_str_ok))))
 	}
 
 	if(reverse){
@@ -26,6 +26,7 @@ solveKinematicChain <- function(joint.types = NULL, joints.unknown = NULL, joint
 		joint.cons <- joint.cons[length(joint.cons):1]
 		joints.dist <- joints.dist[length(joints.dist):1]
 		joints.prev <- joints.prev[nrow(joint.coor):1, ]
+		joint.init <- joint.init[nrow(joint.init):1, ]
 	}
 
 	if(!is.null(joint.types)){
@@ -45,10 +46,102 @@ solveKinematicChain <- function(joint.types = NULL, joints.unknown = NULL, joint
 			r_list <- list()
 			
 			# REVERSE INPUTS
-			if(type_str %in% c('SSpRr', 'SSpLt')){
+			if(type_str %in% c('SSpRr', 'SSpLt', 'SSpPtRrpS')){
 				return(solveKinematicChain(joint.types=joint.types, joints.unknown=joints.unknown, joint.coor=joint.coor, 
-					joint.cons=joint.cons, joints.dist=joints.dist, joints.prev=joints.prev, reverse=TRUE, 
+					joint.cons=joint.cons, joints.dist=joints.dist, joints.prev=joints.prev, joint.init=joint.init, reverse=TRUE, 
 					print.progress=print.progress))
+			}
+
+			if(type_str == 'SRrpPtSpS'){
+
+				if(print.progress) cat(paste0('\t\t\tSolve kinematic chain for ', type_str, '\n'))
+				
+				## MAKE SURE R-JOINT VECTOR IS ORTHOGONAL TO ADJOINING LINKS
+
+				# DEFINE PLANE
+				plane <- list('n'=joint.cons[[3]],'p'=joint.init[3, ])
+	
+				# SET PROJECTED POINTS
+				j_prev_proj <- rbind(joints.prev[c(1,2), ], pointPlaneProj(q=joints.prev[2, ], p=plane$p, n=plane$n), 
+					pointPlaneProj(q=joints.prev[4, ], p=plane$p, n=plane$n), joints.prev[c(4,5), ])
+				j_init_proj <- rbind(joint.init[c(1,2), ], pointPlaneProj(q=joint.init[2, ], p=plane$p, n=plane$n), 
+					pointPlaneProj(q=joint.init[4, ], p=plane$p, n=plane$n), joint.init[c(4,5), ])
+	
+				# INTERJOINT DISTANCE
+				j_init_proj_dist <- distPointToPoint(j_init_proj)
+
+				# CREATE MATRIX FOR NEW POINTS
+				j_proj <- j_init_proj*NA
+				j_proj[c(1,6),] <- joint.coor[c(1,5),]
+
+				# DEFINE CIRCLE TO SOLVE FOR ROTATION OF RPPS LINK
+				j1_proj <- pointPlaneProj(q=joint.coor[1,], p=plane$p, n=plane$n)
+				j1_prev_proj <- pointPlaneProj(q=joint.init[1,], p=plane$p, n=plane$n)
+				rotation_circle <- defineCircle(center=j1_proj, nvector=plane$n, radius=distPointToPoint(j1_prev_proj, j_init_proj[4,]))
+
+				# FIND P2 CIRCLE PROJECTED INTO PLANE
+				j5_proj <- pointPlaneProj(q=joint.coor[5,], p=plane$p, n=plane$n)
+				tri_a <- abs(distPointToPoint(j5_proj, joint.coor[5,])-j_init_proj_dist[4])
+				j4_circle_r <- sqrt(j_init_proj_dist[5]^2 - tri_a^2)
+				j4_circle_plane <- defineCircle(center=j5_proj, nvector=plane$n, radius=j4_circle_r)
+
+				# FIND INTERSECTION CIRCLES
+				intersect_circles <- intersectCircles(j4_circle_plane, rotation_circle)
+	
+				# FIND POINT CLOSEST TO PREVIOUS POINT
+				if(length(intersect_circles) == 3){
+					dist_pp <- c(distPointToPoint(intersect_circles[[1]], j_prev_proj[4,]), distPointToPoint(intersect_circles[[2]], j_prev_proj[4,]))
+					j_proj[4,] <- intersect_circles[[which.min(dist_pp)]]
+				}else if(length(intersect_circles) == 1){
+					j_proj[4,] <- intersect_circles[[1]]
+				}
+
+				# FIND RADIUS OF P1 CIRCLE PROJECTED INTO PLANE
+				tri_a <- abs(distPointToPoint(j1_proj, joint.coor[1, ])-j_init_proj_dist[2])
+				j1_circle_r <- sqrt(j_init_proj_dist[1]^2 - tri_a^2)
+	
+				# FIND PROJECTED CIRCLE OF POSSIBLE P1 POSITIONS IN PLANE
+				j1_circle_plane <- defineCircle(center=j1_proj, nvector=plane$n, radius=j1_circle_r)
+
+				# P3 POSITION ON CIRCLE AT GIVEN DISTANCE FROM P4
+				P3 <- circlePoint(j1_circle_plane, angleOnCircleFromPoint(circle=j1_circle_plane, 
+					dist=j_init_proj_dist[3], P=j_proj[4,]))
+
+				# CHOOSE POINT CLOSEST TO PREVIOUS POINT
+				if(is.matrix(P3)){
+					j_proj[3,] <- P3[which.min(distPointToPoint(P3,j_prev_proj[3,])), ]
+				}else{
+					j_proj[3,] <- P3
+				}
+
+				# FIND J2 AND J5
+				j_proj[2,] <- j_proj[3,] + j_init_proj_dist[2]*uvector(joint.coor[1,]-j1_proj)
+				j_proj[5,] <- j_proj[4,] + j_init_proj_dist[4]*uvector(joint.coor[5,]-j5_proj)
+				
+				# FIND TRANSFORMATIONS
+				translate <- j_proj[2,] - joint.coor[2, ]
+				t_points <- joint.coor[2:4, ] + matrix(translate, nrow=3, ncol=3, byrow=TRUE)
+
+				# FIND ROTATION ANGLE OF RPPS LINK
+				pnl <- pointNormalOnLine(t_points[3, ], t_points[1, ], j_proj[3,])
+				r_angle <- avec(j_proj[5,] - pnl, t_points[3, ] - pnl)
+
+				# ROTATE RPPS LINK
+				r_points <- rotateBody(m=t_points, p=pnl, v=plane$n, a=r_angle)
+				if(sum(abs(r_points[3, ] - j_proj[5,])) > 1e-5){
+					r_points <- rotateBody(m=t_points, p=pnl, v=plane$n, a=-r_angle)
+					r_angle <- -r_angle
+				}
+
+				#print(joint.coor)
+				#print(r_points)
+				#print(j_proj)
+
+				r_list <- list(
+					list('r'=0, 'p'=0),
+					list('t'=translate, 'r'=r_angle, 'p'=pnl),
+					list('p'=0)
+				)
 			}
 			
 			if(type_str == 'RrSpS'){
