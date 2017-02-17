27ff8026c59005d82646d54e02710a1397272adb
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-02-17 13:34:15 -0500

    Fixed incorrect 'return' notation

--- R/cprod.R
+++ R/cprod.R
@@ -9,7 +9,7 @@ cprod <- function(u, v, h='right'){
 	if(is.vector(v) && is.null(dim(v))){
 		if(length(u) != length(v)){
 			cat("CPROD ERROR: VECTORS ARE NOT OF EQUAL DIMENSION\n")
-			return
+			return(rep(NA,3))
 		}
 		if(length(v) == 2){
 			u[3] <- 0
