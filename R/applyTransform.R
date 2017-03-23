36a22d894c4af91294f79692b6ad3512924f7a9d
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-03-23 00:27:03 -0400

    Added function to apply array of transformation matrices

---
+++ R/applyTransform.R
@@ -0,0 +1,53 @@
+applyTransform <- function(pmat, tarr, assoc = NULL){
+
+	# Create array for transformed points
+	parr <- array(NA, dim=c(nrow(pmat), 3, dim(tarr)[3]), dimnames=list(rownames(pmat), NULL, NULL))
+
+	if(length(dim(tarr)) == 3){
+		
+		# Single body
+
+		# Get point coordinates as matrix for transformation - coerce to matrix if single point
+		pcoor <- rbind(matrix(t(pmat), ncol=nrow(pmat)), rep(1, nrow(pmat)))
+		colnames(pcoor) <- rownames(pmat)
+
+		# Apply transformation
+		tcoor <- apply(tarr, 3, '%*%', pcoor)
+
+		# Convert to array
+		tcoor_arr <- array(tcoor, dim=c(4, nrow(pmat), dim(tarr)[3]))
+
+		# Swap first two dimensions (transpose each "matrix" within array) and remove 1s
+		parr <- aperm(tcoor_arr[1:3, , ], perm=c(2,1,3))
+
+	}else{
+
+		# Multiple bodies - apply transformations for each body
+		for(body in 1:dim(tarr)[4]){
+		
+			# Get body name
+			body_name <- dimnames(tarr)[[4]][body]
+
+			# Find points associated with body
+			body_assoc <- which(assoc == body_name)
+		
+			# Skip if no points associated with body
+			if(length(body_assoc) == 0) next
+
+			# Get point coordinates as matrix for transformation - coerce to matrix if single point
+			pcoor <- rbind(matrix(t(pmat[body_assoc, ]), ncol=length(body_assoc)), rep(1, length(body_assoc)))
+			colnames(pcoor) <- rownames(pmat)[body_assoc]
+		
+			# Apply transformation
+			tcoor <- apply(tarr[, , , body], 3, '%*%', pcoor)
+
+			# Convert to array
+			tcoor_arr <- array(tcoor, dim=c(4, length(body_assoc), dim(tarr)[3]))
+
+			# Swap first two dimensions (transpose each "matrix" within array) and remove 1s
+			parr[colnames(pcoor), , ] <- aperm(tcoor_arr[1:3, , ], perm=c(2,1,3))
+		}
+	}
+
+	parr
+}
