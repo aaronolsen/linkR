386020d68e5305f1150ab96e47e56f6f9e269e6f
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-03-27 22:37:39 -0400

    Added function to find error in joint animation for optimization

---
+++ R/animate_joint_error.R
@@ -0,0 +1,10 @@
+animate_joint_error <- function(p, type, cons, coor, coor.compare, lcs = NULL, 
+	check.joint.cons = FALSE, print.progress = FALSE){
+
+	# Run joint model
+	anim <- animate_joint(type=type, cons=cons, param=c(0, p), coor=coor, lcs=lcs, 
+		check.joint.cons=check.joint.cons, print.progress=print.progress)
+
+	# Compare simulated coordinates to ideal
+	sqrt(mean((anim$coor[, , 2] - coor.compare)^2))
+}
