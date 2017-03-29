aa788fe392a5ced68332d9c17074c9a5063ac199
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-03-28 22:13:06 -0400

    Added parameters to centroidSize

--- R/centroidSize.R
+++ R/centroidSize.R
@@ -1,4 +1,4 @@
-centroidSize <- function(m){
+centroidSize <- function(m, na.rm = TRUE, transpose = FALSE){
 
 	if(is.array(m) && length(dim(m)) > 2){
 		r <- rep(NA, dim(m)[3])
@@ -7,17 +7,20 @@ centroidSize <- function(m){
 			n <- na.omit(m[,, i])
 		
 			# CENTER
-			n <- n - matrix(colMeans(n), nrow=nrow(n), ncol=ncol(n), byrow=TRUE)
+			n <- n - matrix(colMeans(n, na.rm=na.rm), nrow=nrow(n), ncol=ncol(n), byrow=TRUE)
 		
 			# FIND CENTROID SIZE
 			r[i] <- sqrt(sum(n^2))
 		}
 		return(r)
 	}else{
+		
+		if(transpose) m <- t(m)
+	
 		m <- na.omit(m)
 	
 		# CENTER
-		m <- m - matrix(colMeans(m), nrow=nrow(m), ncol=ncol(m), byrow=TRUE)
+		m <- m - matrix(colMeans(m, na.rm=na.rm), nrow=nrow(m), ncol=ncol(m), byrow=TRUE)
 	
 		# FIND CENTROID SIZE
 		r <- sqrt(sum(m^2))
