003310b81bf3ae5fec4562352810a932c4c9a459
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-03-29 23:41:26 -0400

    Corrected parameter

--- R/animate_joint_error.R
+++ R/animate_joint_error.R
@@ -2,7 +2,7 @@ animate_joint_error <- function(p, type, cons, coor, coor.compare, lcs = NULL,
 	check.joint.cons = FALSE, print.progress = FALSE){
 
 	# Run joint model
-	anim <- animate_joint(type=type, cons=cons, param=c(0, p), coor=coor, lcs=lcs, 
+	anim <- animate_joint(type=type, cons=cons, param=rbind(rep(0, length(p)), p), coor=coor, lcs=lcs, 
 		check.joint.cons=check.joint.cons, print.progress=print.progress)
 
 	# Compare simulated coordinates to ideal
