b95cdaafc13b9a7d3877d570371909a0777381f1
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-03-29 23:43:22 -0400

    Debugging

--- R/estimRotate.R
+++ R/estimRotate.R
@@ -1,4 +1,5 @@
-estimRotate <- function(coor, coor.align = NULL, max.iter.use = 20, max.pair.use = 20, unique.pairs = FALSE, wt = 'angle'){
+estimRotate <- function(coor, coor.align = NULL, max.iter.use = 20, max.pair.use = 20, 
+	unique.pairs = FALSE, wt = 'angle'){
 
 	## Estimates center of rotation (CoR) and axis of rotation (AoR) for coordinates over 
 	## a series iterations
@@ -59,27 +60,27 @@ estimRotate <- function(coor, coor.align = NULL, max.iter.use = 20, max.pair.use
 	cor_avg <- colSums(cor_mat*wts, na.rm=TRUE) / sum(wts, na.rm=TRUE)
 	aor_avg <- uvector(colSums(aor_mat*wts, na.rm=TRUE) / sum(wts, na.rm=TRUE))
 
-	# Add final non-linear optimization
-
-
-	return(list(
-		CoR=cor_avg,
-		AoR=aor_avg
-	))
 
 	if(FALSE){
 		svg.new(file='Test.html')
 		for(i in 1:dim(coor)[3]) svg.pointsC(coor[, , i], close=TRUE, cex=3)
 
 		svg.points(cor_avg, col='purple', cex=3)
-		svg.arrows(rbind(cor_avg, cor_avg + aor_avg), col='purple')
+		svg.arrows(rbind(cor_avg, cor_avg + 2*aor_avg), col='purple')
 
 		svg.points(cor_mat, col='red', cex=2)
-		for(i in 1:nrow(cor_mat)) svg.arrows(rbind(cor_mat[i,], cor_mat[i,] + 0.5*aor_mat[i,]), col='red')
+		for(i in 1:nrow(cor_mat)) svg.arrows(rbind(cor_mat[i,], cor_mat[i,] + 1*aor_mat[i,]), col='red')
 
-		svg.points(c(0.2,0.5,0.8), col='orange', cex=3)
-		svg.arrows(rbind(c(0.2,0.5,0.8), c(0.2,0.5,0.8)+2*uvector(c(1,0.3,0))), col='orange')
+		svg.points(c(0.1,0.3,0.5), col='orange', cex=3)
+		
+		svg.arrows(rbind(c(0.1,0.3,0.5), c(0.1,0.3,0.5)+2*uvector(c(0,0,1))), col='orange')
+		#svg.arrows(rbind(c(0.1,0.3,0.5), c(0.1,0.3,0.5)+2*uvector(c(1,0,0))), col='orange')
 
 		svg.close()
 	}
+
+	return(list(
+		CoR=cor_avg,
+		AoR=aor_avg
+	))
 }
