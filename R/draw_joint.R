dbb620dc46f9e5713e4329e0789fd2c8285406ab
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-04-01 12:51:27 -0400

    Fixed constraint vector drawing

--- R/draw_joint.R
+++ R/draw_joint.R
@@ -1,5 +1,6 @@
 draw_joint <- function(file, animate.joint, fixed, fixed.lcs, col=c('red', 'green', 'blue'), 
-	cons.col=c('purple', 'MediumPurple', 'Magenta'), cex=c(3,7), lwd=1, draw.lcs = TRUE, 
+	cons.col.r=c('purple', 'MediumPurple', 'Magenta'), cons.col.t=c('blue', 'teal', 'aqua'), 
+	cex=c(3,7), lwd=1, draw.lcs = TRUE, 
 	animate = TRUE, arrow.len=1, head.len='auto', animate.duration=4, window.title='draw_joint', 
 	animate.reverse = TRUE){
 
@@ -93,20 +94,20 @@ draw_joint <- function(file, animate.joint, fixed, fixed.lcs, col=c('red', 'gree
 			for(j in 1:dof[1]){
 				if(animate){
 					for(iter in 1:n_iter) arrow_arr[, , iter] <- rbind(jt_cons[iter, 1:3], jt_cons[iter, 1:3]+0.75*arrow.len*uvector(jt_cons[iter, (j*3+1):(j*3+3)]))
-					svg.arrows(arrow_arr, lwd=lwd, col=cons.col[j], len=head.len, opacity=opacity)
+					svg.arrows(arrow_arr, lwd=lwd, col=cons.col.r[j], len=head.len, opacity=opacity)
 				}else{
 					for(iter in 1:n_iter){
 						svg.arrows(rbind(jt_cons[iter, 1:3], jt_cons[iter, 1:3]+0.75*arrow.len*uvector(jt_cons[iter, (j*3+1):(j*3+3)])), 
-							lwd=lwd, col=cons.col[j], len=head.len, opacity=opacity)
+							lwd=lwd, col=cons.col.r[j], len=head.len, opacity=opacity)
 					}
 				}
 			}
 
 			# Draw point at center of rotation			
-			svg.pointsC(array(t(jt_cons[, 1:3]), dim=c(1,3,nrow(jt_cons))), cex=cex[1], col='purple')
+			svg.pointsC(array(t(jt_cons[, 1:3]), dim=c(1,3,nrow(jt_cons))), cex=cex[1], col=cons.col.r[1])
 
 			# Draw path of center of rotation
-			svg.lines(jt_cons[, 1:3], lwd=lwd, col='purple')
+			svg.lines(jt_cons[, 1:3], lwd=lwd, col=cons.col.r[1])
 		}
 
 		# Draw translational constraints
@@ -114,7 +115,7 @@ draw_joint <- function(file, animate.joint, fixed, fixed.lcs, col=c('red', 'gree
 			if(dof[1] == 0){j_add <- 0}else{j_add <- (dof[1]+1)*3}
 			for(j in 1:dof[2]){
 				for(iter in 1:n_iter) arrow_arr[, , iter] <- rbind(mobile_lcs[1, , iter], mobile_lcs[1, , iter]+0.75*arrow.len*uvector(jt_cons[iter, ((j-1)*3+1+j_add):((j-1)*3+3+j_add)]))
-				svg.arrows(arrow_arr, lwd=1, col=cons.col[j], len=head.len, opacity=opacity)
+				svg.arrows(arrow_arr, lwd=1, col=cons.col.t[j], len=head.len, opacity=opacity)
 			}
 		}
 	}
