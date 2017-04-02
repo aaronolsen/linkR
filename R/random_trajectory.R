7be5b67586d5cee2e508047fa477b9c2304ff24e
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-04-01 22:59:08 -0400

    Adjusted cumulative parameter handling

--- R/random_trajectory.R
+++ R/random_trajectory.R
@@ -1,5 +1,5 @@
 random_trajectory <- function(n, start = 0, mean = 0, sd = 1, seed = NULL, 
-	smooth = TRUE, span = NULL, cumulative = FALSE){
+	smooth = TRUE, span = NULL, cumulative = TRUE){
 
 #	if(!cumulative) n <- n + 1
 
@@ -10,19 +10,21 @@ random_trajectory <- function(n, start = 0, mean = 0, sd = 1, seed = NULL,
 	if(!is.null(seed)) set.seed(seed)
 	
 	# Make sure dimensions match
-	if(length(start) < length(mean)) start <- rep(start, length(mean))
+	if(!is.null(start)) if(length(start) < length(mean)) start <- rep(start, length(mean))
 	if(length(sd) < length(mean)) sd <- rep(sd, length(mean))
 
 	# Create matrix to hold final values
 	rt_mat <- matrix(NA, nrow=n, ncol=length(mean))
 
 	# Run Brownian motion (cumulative sum of normally distributed random variable)
-	if(cumulative){
-		for(i in 1:length(mean)) rt_mat[, i] <- cumsum(rnorm(n, mean=mean[i], sd=sd[i]))
-	}else{
-		for(i in 1:length(mean)) rt_mat[, i] <- rnorm(n, mean=mean[i], sd=sd[i])
-	}
+	for(i in 1:length(mean)) rt_mat[, i] <- cumsum(rnorm(n, mean=mean[i], sd=sd[i]))
+	
+	# Set start
+	if(!is.null(start)) rt_mat <- rt_mat - matrix((rt_mat[1, ]-start), nrow=nrow(rt_mat), ncol=ncol(rt_mat), byrow=TRUE)
 
+	# Find difference between iterations for non-cumulative output
+	if(!cumulative) rt_mat <- apply(rbind(rep(0,ncol(rt_mat)), rt_mat), 2, 'diff')
+		
 	# Smooth
 	if(smooth){
 	
@@ -40,9 +42,6 @@ random_trajectory <- function(n, start = 0, mean = 0, sd = 1, seed = NULL,
 			rt_mat[, i] <- predict(lowpass.loess, data_frame)
 		}
 	}
-	
-	# Set start
-	rt_mat <- rt_mat - matrix((rt_mat[1, ]-start), nrow=nrow(rt_mat), ncol=ncol(rt_mat), byrow=TRUE)
 
 	rt_mat
 }
