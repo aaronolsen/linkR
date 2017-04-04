d40a7dc76d6a25ab149dcbb13775961a9044e177
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-04-03 20:56:27 -0400

    Added parameter to pass checks and removed for-loop

--- R/pointNormalOnLine.R
+++ R/pointNormalOnLine.R
@@ -1,29 +1,30 @@
-pointNormalOnLine <- function(pt, l1, l2=NULL){
+pointNormalOnLine <- function(pt, l1, l2 = NULL, checks = TRUE){
 
 	# FINDS POSITION OF POINT X ON A LINE AT THE MINIMUM DISTANCE FROM POINT X TO THE INPUT POINT
 	# Adapted from : http://paulbourke.net/geometry/pointline/
 
-	return_vector <- FALSE
-	if(is.vector(pt)){
-		pt <- matrix(pt, nrow=1, ncol=length(pt))
-		return_vector <- TRUE
-	}
-
-	r <- matrix(NA, nrow=nrow(pt), ncol=ncol(pt))
-
-	for(i in 1:nrow(pt)){
+	if(checks){
 
 		# IF L2 ARGUMENT IS NULL THEN TREAT L1 AS VECTOR AND GET SECOND POINT ON LINE THROUGH ORIGIN
 		if(is.null(l2)) l2 <- c(0,0,0)
 	
 		# CHECK THAT POINTS DEFINING LINE ARE NOT COINCIDENT
 		if(sum((l2 - l1)^2) == 0) return(NA)
-	
-		# SOLVE FOR POSITION OF POINT ON LINE
-		u <- sum((pt[i, ] - l1)*(l2 - l1)) / sum((l2 - l1)^2)
-		r[i, ] <- l1 + u*(l2 - l1)
 	}
 
-	if(return_vector) return(as.vector(r))
+	# Convert to matrices
+	if(is.vector(pt)) pt <- matrix(pt, 1, length(pt))
+
+	# Convert to matrix
+	nrow_pt <- nrow(pt)
+	l1 <- matrix(l1, nrow_pt, 3, byrow=TRUE)
+	l2 <- matrix(l2, nrow_pt, 3, byrow=TRUE)
+
+	# Solve for position of point on line
+	u <- rowSums((pt - l1)*(l2 - l1)) / rowSums((l2 - l1)^2)
+
+	# Solve for points on line
+	r <- l1 + u*(l2 - l1)
+	
 	r
 }
