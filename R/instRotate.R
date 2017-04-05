fd54b3cd1a6901822e4e864d67acbe84d82ad9d0
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-04-04 23:42:09 -0400

    Added AoR-only fitting option

--- R/instRotate.R
+++ R/instRotate.R
@@ -1,4 +1,4 @@
-instRotate <- function(m1, m2, na.rm = FALSE, positive.only = TRUE, CoR = NULL){
+instRotate <- function(m1, m2, na.rm = FALSE, positive.only = TRUE, aor.only = FALSE, AoR = NULL, CoR = NULL){
 
 	## Finds instantaneous center of rotation (CoR) and axis of rotation (AoR) between 
 	## coordinates in two different orientations. Four possible solutions can be obtained 
@@ -8,7 +8,8 @@ instRotate <- function(m1, m2, na.rm = FALSE, positive.only = TRUE, CoR = NULL){
 	##    2) Returns the minimum of possible positive angles
 	## A single axis of rotation can describe any number and combination of rotations about
 	## the same center of rotation but not more than one rotation about different centers
-	## of rotation.
+	## of rotation. The CoR is necessary to identify the correct sign of the rotation 
+	## angle. If is known (even approximately) it can be provided to reduce run time.
 
 	# Remove rows with NA values
 	if(na.rm){
@@ -37,11 +38,16 @@ instRotate <- function(m1, m2, na.rm = FALSE, positive.only = TRUE, CoR = NULL){
 	# Find rotation matrix
 	rmat <- svd$v %*% S %*% t(svd$u)
 
-	# Compute eigen vectors and values
-	rmat_eigen <- eigen(rmat)
+	if(is.null(AoR)){
 
-	# Find AoR as the eigenvector corresponding to eigenvalue closest to 1 (truncation errors)
-	AoR <- Re(rmat_eigen$vectors[, which.min(abs(Re(rmat_eigen$values) - 1))])
+		# Compute eigen vectors and values
+		rmat_eigen <- eigen(rmat)
+
+		# Find AoR as the eigenvector corresponding to eigenvalue closest to 1 (truncation errors)
+		AoR <- Re(rmat_eigen$vectors[, which.min(abs(Re(rmat_eigen$values) - 1))])
+	}
+
+	if(aor.only) return(list('AoR'=AoR))
 
 	# Get rotation magnitude/angle
 	acos_val <- (sum(rmat*diag(3)) - 1) / 2
@@ -50,46 +56,44 @@ instRotate <- function(m1, m2, na.rm = FALSE, positive.only = TRUE, CoR = NULL){
 	if(abs(acos_val) > 1) acos_val <- sign(acos_val)
 
 	# Find angle from acos value
-	angle <- rep(acos(acos_val), 4)*c(1,1,-1,-1)
+	angle <- acos(acos_val)
+	
+	# Try both angles
+	rmatch <- c(sum(abs(m1c %*% tMatrixEP(AoR, angle) - m2c)), sum(abs(m1c %*% tMatrixEP(AoR, -angle) - m2c)))
 
+	# Keep angle with lowest/zero error
+	angle <- (c(1,-1)*angle)[which.min(rmatch)]
+
+	# Find or set center of rotation
 	if(is.null(CoR)){
-
+	
 		# Find distance from mid-centroid point to CoR
-		dcen <- -(distPointToPoint(m1_centroid, m2_centroid) / 2) / tan(angle[1]/2)
+		dcen <- -(distPointToPoint(m1_centroid, m2_centroid) / 2) / tan(abs(angle)/2)
 
 		# Find vector to the CoR
 		v2rcen <- cprod(m2_centroid - m1_centroid, AoR)
 
 		# Find possible centers of rotation
-		rcen <- matrix(colMeans(rbind(m1_centroid, m2_centroid)), 4, 3, byrow=TRUE) + 
-			matrix(dcen*uvector(v2rcen), 4, 3, byrow=TRUE)*matrix(rep(c(-1,1), 6), 4, 3)
+		rcen <- matrix(colMeans(rbind(m1_centroid, m2_centroid)), 2, 3, byrow=TRUE) + 
+			matrix(dcen*uvector(v2rcen), 2, 3, byrow=TRUE)*matrix(rep(c(-1,1), 6), 2, 3)
+
+		# Try both potential CoRs
+		rmatch <- c(sum(abs(rotateBody(m1, rcen[1, ], AoR, angle) - m2)), sum(abs(rotateBody(m1, rcen[2, ], AoR, angle) - m2)))
+
+		# Find center with lowest difference from actual body position
+		rcen <- rcen[which.min(rmatch), ]
 
 	}else{
 		
 		# Set center
-		rcen <- matrix(CoR, nrow=2, ncol=3, byrow=TRUE)
-		
-		# Only test positive and negative angle (because center is already determined)
-		angle <- angle[c(1,3)]
+		rcen <- CoR
 	}
-
-	# Try each CoR and angle on original m1
-	nrow_rcen <- nrow(rcen)
-	m1r <- array(NA, dim=c(dim(m1)[1:2], nrow_rcen))
-
-	# Try all combinations with potential CoR
-	for(i in 1:nrow_rcen) m1r[, , i] <- rotateBody(m1, rcen[i, ], AoR, angle[i])
-
-	# Find combination with lowest difference from actual body position
-	#print(apply(abs(m1r - array(m2, dim=c(dim(m1)[1:2], nrow_rcen))), 3, 'sum'))
-	min_idx <- which.min(apply(abs(m1r - array(m2, dim=c(dim(m1)[1:2], nrow_rcen))), 3, 'sum'))
-
-	# Save only that combination
-	rcen <- rcen[min_idx, ]
-	angle <- angle[min_idx]
 
 	# If angle is negative, change angle and AoR sign
 	if(positive.only && angle < 0){angle <- -angle; AoR <- -AoR}
+
+	# Slide CoR along AoR to closest point on AoR from m1 centroid
+	#rcen <- pointNormalOnLine(pt=m1_centroid, l1=rcen, l2=rcen + AoR)
 
 	# Find error as mean distance between points
 	#mean_error <- mean(sqrt(rowSums((rotateBody(m1, p=rcen, v=AoR, a=angle) - m2)^2))); print(mean_error)
