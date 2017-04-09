e50f37d72426a153a26b25b8ddc49db3ff6df321
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-04-08 21:38:28 -0400

    Updated error function for constraint optimization

--- R/constraint_error.R
+++ R/constraint_error.R
@@ -1,8 +1,11 @@
-constraint_error <- function(p, type, param, coor, coor.compare, lcs = NULL, 
+constraint_error <- function(p, type, param, coor, coor.compare, joint.cons.init, dof, lcs = NULL, 
 	check.joint.cons = FALSE, print.progress = FALSE){
 
+	# Apply transformations to constraint parameters
+	cons <- apply_cons_optim_transforms(p, joint.cons.init, type, dof)
+
 	# Run joint model
-	anim <- animate_joint(type=type, cons=p, param=param, coor=coor, lcs=lcs, 
+	anim <- animate_joint(type=type, cons=cons, param=param, coor=coor, lcs=lcs, 
 		check.joint.cons=check.joint.cons, print.progress=print.progress)
 
 	# Compare simulated coordinates to ideal
