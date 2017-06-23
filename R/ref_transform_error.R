47d1528176488190100a7760bde6db85811a68c1
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-06-22 23:51:22 -0400

    Added error function for optimizing transformation of transformed points

---
+++ R/ref_transform_error.R
@@ -0,0 +1,16 @@
+ref_transform_error <- function(p, ref.points, fit.points, transform){
+
+	# Create test transformation matrix from p
+	tmat <- diag(4)
+	tmat[1:3, 1:3] <- rotationMatrixZYX(p[1:3])
+	tmat[1:3, 4] <- p[4:6]
+
+	# Apply test transformation to reference points
+	ref.points <- applyTransform(ref.points, tmat)	
+
+	# Apply transformation array to test transformed reference points
+	ref.points <- applyTransform(ref.points, transform)	
+
+	# Return error
+	return(sqrt(mean((ref.points - fit.points)^2)))
+}
