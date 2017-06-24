d5cc3cd61254df3a8d843dcac53533a5efbec560
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-06-24 12:10:06 -0400

    Added new optimization parameter types

--- R/animate_mechanism_error.R
+++ R/animate_mechanism_error.R
@@ -1,21 +1,30 @@
 animate_mechanism_error <- function(p, fit.points, mechanism, 
-	input.param, input.joint, input.body, replace, iter){
+	input.param, input.joint, input.body, replace, n.input = NULL, n.cons = NULL){
 
 	# Replace parameter with optimize parameters
 	if(replace == 'input.param'){
 		n <- 1
 		for(i in 1:length(input.param)){
-			n_inputs <- ncol(input.param[[i]])
-			input.param[[i]] <- matrix(p[n:(n + n_inputs - 1)], nrow=1, ncol=n_inputs)
-			n <- n + n_inputs
+			input.param[[i]] <- matrix(p[n:(n + n.input[i] - 1)], nrow=1, ncol=n.input[i])
+			n <- n + n.input[i]
+		}
+	}else if(replace == 'joint.coor'){
+		mechanism$joint.coor <- matrix(p, mechanism$num.joints, 3, byrow=TRUE)
+	}else if(replace == 'joint.cons'){
+		i_params <- 1
+		for(i in 1:mechanism$num.joints){
+			if(n.cons[i] == 0) next
+			mechanism$joint.cons[[i]][, , 1] <- p[i_params:(i_params+n.cons[i]-1)]
+			i_params <- i_params + n.cons[i]
 		}
 	}
 	
 	# Run mechanism model
-	anim_mech <- animateMechanism(mechanism, input.param=input.param, input.joint=input.joint, 
+	anim_mech <- suppressWarnings(animateMechanism(mechanism, input.param=input.param, input.joint=input.joint, 
 		input.body=input.body, print.progress=FALSE, check.inter.joint.dist=FALSE, 
-		check.joint.cons=FALSE)
-
+		check.joint.cons=FALSE))
+	
 	# Compare simulated coordinates to ideal
-	sqrt(mean((anim_mech$body.points[, , 1] - fit.points)^2))
+	if(dim(anim_mech$body.points)[3] == 1) return(sqrt(mean((anim_mech$body.points[, , 1] - fit.points)^2)))
+	return(sqrt(mean((anim_mech$body.points - fit.points)^2)))
 }
