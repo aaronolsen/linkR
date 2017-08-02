202a3fc5848579ca250d0acbaa569bd86c156399
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-08-01 21:30:11 -0400

    Removed cbind - increases speed?

--- R/mtransform.R
+++ R/mtransform.R
@@ -1,3 +1,15 @@
 mtransform <- function(mat, tmat){
-	mat <- t(tmat %*% t(cbind(mat, rep(1, nrow(mat)))))[, 1:3]
+	
+	if(!is.matrix(mat)){
+		mat <- matrix(mat, nrow=1, ncol=length(mat))
+		dimnames_mat <- NULL
+	}else{
+		dimnames_mat <- dimnames(mat)
+	}
+
+	pmat <- matrix(1, nrow(mat), ncol(mat)+1)
+	pmat[, 1:3] <- mat
+	mat <- t(tmat %*% t(pmat))[, 1:3]
+	dimnames(mat) <- dimnames_mat
+	mat
 }
