c97196b7fc4e5eec08488e09e210e53a89782163
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-10-20 10:16:13 -0400

    Add option to input multiple angles and return an array

--- R/rotateBody.R
+++ R/rotateBody.R
@@ -1,6 +1,17 @@
 rotateBody <- function(m, v, a, p = NULL){
 
 	if(is.vector(m)) m <- matrix(m, nrow=1, ncol=length(m))
+	
+	if(length(a) > 1){
+
+		# Convert matrix to array with same number of iterations as angles
+		m_arr <- array(NA, dim=c(dim(m), length(a)), dimnames=list(dimnames(m)[[1]], dimnames(m)[[2]], NULL))
+
+		# Rotate points at each iteration
+		for(iter in 1:length(a)) m_arr[,,iter] <- rotateBody(m=m, v=v, a=a[iter], p=p)
+
+		return(m_arr)
+	}
 
 	# IF INPUT MATRIX HAS NO ROWS, RETURN EMPTY MATRIX
 	if(dim(m)[1] == 0) return(m)
