10049b6d9f5e6af03d141d43274f753ae6adf520
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-10-25 23:54:37 -0400

    Added weighted mean option

--- R/dist_point_to_line_error.R
+++ R/dist_point_to_line_error.R
@@ -1,6 +1,6 @@
-dist_point_to_line_error <- function(p, l1, l2){
+dist_point_to_line_error <- function(p, l1, l2, wts){
 	n_lines <- nrow(l1)
 	d <- rep(NA, n_lines)
 	for(i in 1:n_lines) d[i] <- distPointToLine(p, l1[i,], l2[i,])
-	mean(d)
+	mean(wts*d)
 }
