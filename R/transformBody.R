a9c48f8ce5c1d9f8e93eb705eb44d268aeef7849
Aaron Olsen <aaronolsen@users.noreply.github.com>
2018-02-05 08:25:48 -0500

    Updated transformations and print progress

--- R/transformBody.R
+++ R/transformBody.R
@@ -1,9 +1,13 @@
-transformBody <- function(mechanism, status.solved.to, body, tmat, iter, print.progress, indent){
+transformBody <- function(mechanism, body, tmat, iter, status.solved.to = NULL, 
+	at.joint = NULL, print.progress = FALSE, indent = '\t', indent.level = 3){
 
-	if(print.progress) cat(paste0(paste0(rep(indent, 3), collapse=''), 'transformBody()\n'))
+	if(print.progress) cat(paste0(paste0(rep(indent, indent.level), collapse=''), 'transformBody() '))
+
+	# Save transformation as last transformation
+	#mechanism[['last.tmat']][, , body, iter] <- tmat
 
 	# Apply transformation to current body transformation
-	mechanism[['body.tmat']][, , body, iter] <- tmat %*% mechanism[['body.tmat']][, , body, iter]
+	mechanism[['tmat']][, , body, iter] <- tmat %*% mechanism[['tmat']][, , body, iter]
 
 	# Find associated joints and sets
 	body_joints <- mechanism[['joint.transform']][[body]]
@@ -24,16 +28,23 @@ transformBody <- function(mechanism, status.solved.to, body, tmat, iter, print.p
 			jt_idx <- body_joints[i]
 			
 			# Change statuses
-			if(!jt_idx %in% mechanism[['fixed.joints']]){
+			if(!jt_idx %in% at.joint){
 				mechanism[['status']][['jointed']][jt_idx] <- FALSE
 				mechanism[['status']][['transformed']][jt_idx, jt_set[i]] <- TRUE
 			}
 
 			# Set joint solved status
-			mechanism[['status']][['solved']][[jt_idx, jt_set[i]]] <- status.solved.to
+			if(!is.null(status.solved.to)){
+				if(!is.null(at.joint) && jt_idx %in% at.joint){
+					if(status.solved.to > mechanism[['status']][['solved']][jt_idx, 1]) mechanism[['status']][['solved']][jt_idx, 1] <- status.solved.to
+					if(status.solved.to > mechanism[['status']][['solved']][jt_idx, 2]) mechanism[['status']][['solved']][jt_idx, 2] <- status.solved.to
+				}else{
+					if(status.solved.to > mechanism[['status']][['solved']][jt_idx, jt_set[i]]) mechanism[['status']][['solved']][jt_idx, jt_set[i]] <- status.solved.to
+				}
+			}
 
 			# Apply to joints associated with body
-			mechanism[['joint.coor.anim']][jt_idx, , iter, jt_set[i]] <- applyTransform(mechanism[['joint.coor']][jt_idx, ], tmat)
+			mechanism[['joint.coor.anim']][jt_idx, , iter, jt_set[i]] <- applyTransform(mechanism[['joint.coor']][jt_idx, ], mechanism[['tmat']][, , body, iter])
 
 			# Skip if no constraint
 			if(is.null(mechanism[['joint.cons']][[jt_idx]])) next
@@ -46,15 +57,16 @@ transformBody <- function(mechanism, status.solved.to, body, tmat, iter, print.p
 			cons_point <- cons_origin + mechanism[['joint.cons']][[jt_idx]]
 			
 			# Transform origin and vectors
-			cons_origin_trfm <- applyTransform(cons_origin, tmat)
-			cons_point_trfm <- applyTransform(cons_point, tmat)
+			cons_origin_trfm <- applyTransform(cons_origin, mechanism[['tmat']][, , body, iter])
+			cons_point_trfm <- applyTransform(cons_point, mechanism[['tmat']][, , body, iter])
 			
 			# Move back to origin
 			mechanism[['joint.cons.anim']][[jt_idx]][, , iter, jt_set] <- cons_point_trfm - cons_origin_trfm
 		}
 
 		if(print.progress){
-			cat(paste0(paste0(rep(indent, 4), collapse=''), 'Transform joints: ', paste0(apply_to_joints, collapse=', ')))
+			#cat(paste0(paste0(rep(indent, indent.level+1), collapse=''), 'Transform body \'', mechanism[['body.names']][body], '\' (', body, ') and associated joints: ', paste0(apply_to_joints, collapse=', ')))
+			cat(paste0('\'', mechanism[['body.names']][body], '\' (', body, ') and associated joints: ', paste0(apply_to_joints, collapse=', ')))
 		}
 	}
 
