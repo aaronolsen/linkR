748826c89acc1ee8cd06dd71df2148a9527adde1
Aaron Olsen <aaronolsen@users.noreply.github.com>
2018-02-12 23:37:24 -0500

    Streamlined function and improved progress printing

--- R/findOpenJoints.R
+++ R/findOpenJoints.R
@@ -1,6 +1,6 @@
 findOpenJoints <- function(joint.conn, body.conn, fixed.joints, body.names = NULL, indent = '\t', indent.level = 1, print.progress = FALSE){
 
-	if(print.progress) cat(paste0(paste0(rep(indent, indent.level), collapse=''), 'findOpenJoints()\n'))
+	if(print.progress) cat(paste0(paste0(rep(indent, indent.level), collapse=''), 'findOpenJoints()'))
 
 	# Get number of joints
 	num_joints <- nrow(body.conn)
@@ -22,91 +22,65 @@ findOpenJoints <- function(joint.conn, body.conn, fixed.joints, body.names = NUL
 	}
 
 	if(print.progress){
-		cat(paste0(paste0(rep(indent, indent.level+1), collapse=''), 'Sole joints (joints that are the only joint connected to a particular body):'))
+		cat(paste0('\n', paste0(rep(indent, indent.level+1), collapse=''), 'Sole joints (joints that are the only joint connected to a particular body):'))
 		if(length(sole_joints) == 0){
-			cat(' none\n')
+			cat(' none')
 		}else{
-			cat(paste0(' ', paste0(sole_joints, collapse=', '), '\n'))
+			cat(paste0(' ', paste0(sole_joints, collapse=', ')))
 		}
 	}
 
+	## Find open joints
 	# Vector to save which joints are open
 	is_open <- rep(TRUE, num_joints)
 
 	# For each joint
 	for(joint_start in 1:num_joints){
 	
-		if(print.progress) cat(paste0(paste0(rep(indent, indent.level+1), collapse=''), 'Starting at joint ', joint_start, '\n'))
+		if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+1), collapse=''), 'Starting at joint ', joint_start))
 
 		#
 		if(joint_start %in% sole_joints){
-			if(print.progress) cat(paste0(paste0(rep(indent, indent.level+2), collapse=''), 'Joint is the only joint connected to a particular body, joint ', joint_start, ' is open\n'))
+			if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+2), collapse=''), 'Joint is the only joint connected to a particular body, joint ', joint_start, ' is open'))
 			next
 		}
 
-		# Start list of joint chain
-		joint_chain <- list()
-		body_chain <- list()
+		# Start list of joint path
+		joint_path <- list(joint_start)
+		body_path <- list()
 
-		# Get adjacent joints
-		adj_joints <- joint.conn[[joint_start]]
+		# Set joint path indices for looping
+		joint_path_idx <- 1:length(joint_path)
 
-		# If joint is fixed, remove any adjacent fixed joints (don't go through fixed body)
-		if(joint_start %in% fixed.joints) adj_joints <- adj_joints[!adj_joints %in% fixed.joints]
-
-		# If only fixed adjacent joints, joint must be open
-		if(length(adj_joints) == 0){
-			if(print.progress) cat(paste0(paste0(rep(indent, indent.level+2), collapse=''), 'Only fixed adjacent joints, joint ', joint_start, ' is open\n'))
-			next
-		}
-
-		# For each adjacent joint
-		for(i in 1:length(adj_joints)){
-			
-			# Create new joint chain
-			joint_chain[[i]] <- c(joint_start, adj_joints[i])
-
-			# Get bodies connected to joints
-			body_table <- table(body.conn[c(joint_start, adj_joints[i]), ])
-
-			# Find body connected by both joints
-			body_chain[[i]] <- as.numeric(names(body_table)[body_table == 2])
-			
-			if(adj_joints[i] %in% fixed.joints){
-				joint_chain[[i]] <- c(joint_chain[[i]], 0)
-				body_chain[[i]] <- c(body_chain[[i]], 1)
-			}
-		}
-		
-		# If fixed joint add chain with 0
-		if(joint_start %in% fixed.joints){
-			joint_chain[[length(joint_chain)+1]] <- c(joint_start, 0)
-			body_chain[[length(body_chain)+1]] <- 1
-		}
-		
 		it <- 0
 		while(it < 100){
+
+			if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+2), collapse=''), it, ':'))
 
 			# Logical to test whether something has changed
 			something_changed <- FALSE
 			
 			#
-			for(i in 1:length(joint_chain)){
+			for(i in joint_path_idx){
+
+				if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+3), collapse=''), i, ') '))
 			
 				# Get last joint
-				last_joint <- tail(joint_chain[[i]], 1)
+				last_joint <- tail(joint_path[[i]], 1)
 
 				# If NA, skip
-				if(is.na(last_joint)) next
-				if(last_joint == 0) next
+				if(last_joint == 0){
+					if(print.progress) cat(paste0(paste0(joint_path[[i]], collapse=','), ' (', paste0(body.names[body_path[[i]]], collapse=','), ')'))
+					next
+				}
 				
 				# Find adjacent joints
 				adj_joints <- joint.conn[[last_joint]]
 				
-				# Remove any joints already in chain
-				adj_joints <- adj_joints[!adj_joints %in% joint_chain[[i]]]
+				# Remove any joints already in path
+				adj_joints <- adj_joints[!adj_joints %in% joint_path[[i]]]
 
-				# Remove any adjacent joints that are adjacent to last two joints in chain
+				# Remove any adjacent joints that are adjacent to last two joints in path
 				# Prevent search from getting more than two joints attached the same body
 				if(length(adj_joints) > 0){
 
@@ -119,8 +93,8 @@ findOpenJoints <- function(joint.conn, body.conn, fixed.joints, body.names = NUL
 						# Find body connected by both joints
 						add_body <- as.numeric(names(body_table)[body_table == 2])
 
-						# If body is already in chain, mark adjacent joint as NA to skip						
-						if(add_body %in% body_chain[[i]]) adj_joints[j] <- NA
+						# If body is already in path, mark adjacent joint as NA to skip						
+						if(length(body_path) > 0 && add_body %in% body_path[[i]]) adj_joints[j] <- NA
 					}
 
 					# Remove any NAs
@@ -130,17 +104,19 @@ findOpenJoints <- function(joint.conn, body.conn, fixed.joints, body.names = NUL
 				# Mark that something changed
 				something_changed <- TRUE
 
-				# Add NA to stop chain
+				# Add NA to stop path
 				if(length(adj_joints) == 0){
-					joint_chain[[i]][length(joint_chain[[i]])+1] <- NA
+					joint_path[[i]][length(joint_path[[i]])+1] <- NA
+					joint_path_idx <- joint_path_idx[joint_path_idx != i]
+					if(print.progress) cat(paste0(paste0(joint_path[[i]], collapse=','), ' (', paste0(body.names[body_path[[i]]], collapse=','), ')'))
 					next
 				}
 				
-				# Set chains before any additions
-				joint_chain_i <- joint_chain[[i]]
-				body_chain_i <- body_chain[[i]]
+				# Set paths before any additions
+				joint_path_i <- joint_path[[i]]
+				if(length(body_path) == 0){ body_path_i <- NULL }else{ body_path_i <- body_path[[i]] }
 			
-				# Then create new chains with the rest
+				# Then create new paths with the rest
 				for(j in 1:length(adj_joints)){
 
 					# Get body to add
@@ -154,60 +130,75 @@ findOpenJoints <- function(joint.conn, body.conn, fixed.joints, body.names = NUL
 					
 					# Add 0 if fixed
 					if(add_joint %in% fixed.joints){
-						add_joint <- c(add_joint, 0)
-						add_body <- c(add_body, 1)
+						if(last_joint %in% fixed.joints){
+							add_joint <- 0
+							add_body <- 1
+						}else{
+							add_joint <- c(add_joint, 0)
+							add_body <- c(add_body, 1)
+						}
 					}
 					
-					# Add first joint to chain, create new chains with any subsequent joints
-					if(j == 1){ add_at <- i }else{ add_at <- length(joint_chain)+1 }
+					# Add first joint to path, create new paths with any subsequent joints
+					if(j == 1){
+						add_at <- i
+					}else{
+						add_at <- length(joint_path)+1
+						joint_path_idx <- c(joint_path_idx, add_at)
+					}
 
-					joint_chain[[add_at]] <- c(joint_chain_i, add_joint)
-					body_chain[[add_at]] <- c(body_chain_i, add_body)
+					# If reached fixed, mark path as done
+					if(tail(add_joint, 1) == 0) joint_path_idx <- joint_path_idx[joint_path_idx != add_at]
+
+					# Add to lists
+					joint_path[[add_at]] <- c(joint_path_i, add_joint)
+					if(is.null(body_path_i)){
+						body_path[[add_at]] <- add_body
+					}else{
+						body_path[[add_at]] <- c(body_path_i, add_body)
+					}
+
+					if(print.progress){
+						if(add_at == i){
+							cat(paste0(paste0(joint_path[[add_at]], collapse=','), ' (', paste0(body.names[body_path[[add_at]]], collapse=','), ')'))
+						}else{
+							if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+3), collapse=''), add_at, ') '))
+							cat(paste0(paste0(joint_path[[add_at]], collapse=','), ' (', paste0(body.names[body_path[[add_at]]], collapse=','), ')'))
+						}
+					}
 				}
 			}
 			
 			# Find number of paths that end in 0
-			chain_end <- unlist(lapply(joint_chain, tail, 1))
+			path_end <- unlist(lapply(joint_path, tail, 1))
 			
-			# If more than one chain 
-			if(sum(chain_end == 0, na.rm=TRUE) > 1){
+			# If more than one path 
+			if(sum(path_end == 0, na.rm=TRUE) > 1){
 
 				# Paths to fixed link
-				end_0_chains <- which(chain_end == 0)
+				end_0_paths <- which(path_end == 0)
 				
-				#print(joint_chain[end_0_chains])
-				#print(body_chain[end_0_chains])
-				#cat('********\n')
-
 				# Get first bodies from all paths to fixed link
-				first_bodies <- unlist(lapply(body_chain[end_0_chains], head, 1))
+				first_bodies <- unlist(lapply(body_path[end_0_paths], head, 1))
 				
 				# If more than one unique body, then joint is open
 				if(length(unique(first_bodies)) > 1){
 					is_open[joint_start] <- FALSE
-					if(print.progress) cat(paste0(paste0(rep(indent, indent.level+2), collapse=''), 'More than one body leading to fixed, joint ', joint_start, ' is closed\n'))
+					if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+2), collapse=''), 'More than one body leading to fixed, joint ', joint_start, ' is closed'))
 					break
 				}
 			}
 			
-			if(!something_changed) break
+			if(!something_changed){
+				if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+3), collapse=''), 'No change'))
+				break
+			}
 			
 			it <- it + 1
 		}
-
-		if(print.progress){
-			for(i in 1:length(joint_chain)){
-				cat(paste0(paste0(rep(indent, indent.level+2), collapse=''), paste0(joint_chain[[i]], collapse=','), '\n'))
-				if(is.null(body.names)){
-					cat(paste0(paste0(rep(indent, indent.level+3), collapse=''), paste0(body_chain[[i]], collapse=','), '\n'))
-				}else{
-					cat(paste0(paste0(rep(indent, indent.level+3), collapse=''), paste0(body.names[body_chain[[i]]], collapse=','), '\n'))
-				}
-			}
-		}
-		
-		#break
 	}
+
+	if(print.progress) cat('\n')
 
 	which_open <- which(is_open)
 	
