b57d84be324a7aaf086c59325e129194c05a5b2c
Aaron Olsen <aaronolsen@users.noreply.github.com>
2018-02-13 22:56:14 -0500

    Added new solvable path

--- R/linkR_sp.R
+++ R/linkR_sp.R
@@ -4,6 +4,7 @@ linkR_sp[['str']] <- list()
 linkR_sp[['exp']] <- list()
 linkR_sp[['exp']][[1]] <- c('R', 'J', 'SN', '1', 'R', 'J', 'NN', '2', 'R', 'D', 'NS')
 linkR_sp[['exp']][[length(linkR_sp[['exp']])+1]] <- c('S', 'J', 'SP', '1', 'S', 'J', 'PS', '1', 'S', 'J', 'PN', '2', 'S', 'D', 'NS')
+linkR_sp[['exp']][[length(linkR_sp[['exp']])+1]] <- c('R', 'J', 'SN', '1', 'S', 'J', 'NN', '2', 'S', 'D', 'NS')
 
 for(linkR_i in 1:length(linkR_sp[['exp']])){
 	
