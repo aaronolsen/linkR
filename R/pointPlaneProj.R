b2322bfbf9b7b1785092de207e8dfaed39719fe6
Aaron Olsen <aaronolsen@users.noreply.github.com>
2018-10-18 18:56:38 -0400

    Added matrix option

--- R/pointPlaneProj.R
+++ R/pointPlaneProj.R
@@ -1,5 +1,14 @@
 pointPlaneProj <- function(q, p, n){
 
+	if(is.matrix(q)){
+		q_ret <- q
+		for(i in 1:nrow(q)){
+			if(is.na(q[i,1])) next
+			q_ret[i,] <- pointPlaneProj(q[i,], p=p, n=n)
+		}
+		return(q_ret)
+	}
+
 	if(q[1] == "non-coincident"){
 		warning("Input point non-coincident")
 		return(c(NA, NA, NA))
