f73ab8b0500eea772dd4fa37d0d5308cd0bc9b06
Aaron Olsen <aaronolsen@users.noreply.github.com>
2019-04-12 14:51:48 -0400

    Added triangle shape

--- R/createPointShape.R
+++ R/createPointShape.R
@@ -2,25 +2,41 @@ createPointShape <- function(shape = c('triangle', 'rectangle'), center = NULL,
 	ends = NULL, width = NULL, nvector = NULL, with.center = FALSE, size = 1){
 
 	if(shape[1] == 'triangle'){
-
-		# Find second vector normal to normal vector
-		v1 <- uvector(vorthogonal(nvector))
-
-		# Length of side
-		h <- size
-
-		# Find distance from triangle center to vertices
-		a <- 2*((sqrt(h^2 - (h/2)^2)) / 3)
 	
-		# Define triangle vertices
-		verts <- rbind(
-			center + a*v1,
-			center + a*(v1 %*% tMatrixEP(nvector, (2*pi)/3)),
-			center + a*(v1 %*% tMatrixEP(nvector, (4*pi)/3))
-		)
+		#
+		if(is.null(ends)){
+
+			# Find second vector normal to normal vector
+			v1 <- uvector(vorthogonal(nvector))
+
+			# Length of side
+			h <- size
+
+			# Find distance from triangle center to vertices
+			a <- 2*((sqrt(h^2 - (h/2)^2)) / 3)
 	
-		if(with.center) verts <- rbind(center, verts)
+			# Define triangle vertices
+			verts <- rbind(
+				center + a*v1,
+				center + a*(v1 %*% tMatrixEP(nvector, (2*pi)/3)),
+				center + a*(v1 %*% tMatrixEP(nvector, (4*pi)/3))
+			)
+	
+			if(with.center) verts <- rbind(center, verts)
 		
+		}else{
+
+			#if(is.null(center) && !is.null(ends)) center <- colMeans(ends)
+			#if(is.null(nvector) && !is.null(ends)){
+			#	nvector1 <- cprod(ends[1,]-center, ends[2,]-center)
+			#	nvector2 <- cprod(ends[2,]-center, ends[3,]-center)
+			#	nvector <- colMeans(rbind(nvector1, nvector2))
+			#}
+
+			# Define triangle vertices
+			verts <- ends
+		}
+
 		connect <- list(c(1,2), c(2,3), c(1,3))
 
 	}else if(shape[1] %in% c('rect', 'rectangle')){
