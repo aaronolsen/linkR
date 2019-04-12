43df771b724e97964f09570efb01bc5819a7d743
Aaron Olsen <aaronolsen@users.noreply.github.com>
2019-04-12 14:52:58 -0400

    Added new solvable paths

--- R/linkR_sp.R
+++ R/linkR_sp.R
@@ -5,9 +5,12 @@ linkR_sp[['exp']] <- list()
 linkR_sp[['exp']][[1]] <- c('R', 'J', 'SN', '1', 'R', 'J', 'NN', '2', 'R', 'D', 'NS')
 linkR_sp[['exp']][[length(linkR_sp[['exp']])+1]] <- c('S', 'J', 'SP', '1', 'S', 'J', 'PS', '1', 'S', 'J', 'PN', '2', 'S', 'D', 'NS')
 linkR_sp[['exp']][[length(linkR_sp[['exp']])+1]] <- c('R', 'J', 'SN', '1', 'S', 'J', 'NN', '2', 'S', 'D', 'NS')
+linkR_sp[['exp']][[length(linkR_sp[['exp']])+1]] <- c('R', 'J', 'SN', '1', 'S', 'J', 'NN', '2', 'S', 'D', 'SN')
 linkR_sp[['exp']][[length(linkR_sp[['exp']])+1]] <- c('R', 'J', 'SN', '1', 'L', 'J', 'NN', '2', 'R', 'D', 'NS')
 linkR_sp[['exp']][[length(linkR_sp[['exp']])+1]] <- c('S', 'J', 'SN', '1', 'L', 'J', 'NN', '2', 'S', 'D', 'NS')
 linkR_sp[['exp']][[length(linkR_sp[['exp']])+1]] <- c('R', 'J', 'SN', '1', 'S', 'J', 'NN', '2', 'P', 'D', 'NS')
+linkR_sp[['exp']][[length(linkR_sp[['exp']])+1]] <- c('P', 'D', 'SN', '1', 'S', 'J', 'NN', '2', 'U', 'J', 'NS', '3', 'S', 'D', 'SN', '4', 'S', 'J', 'NN')
+linkR_sp[['exp']][[length(linkR_sp[['exp']])+1]] <- c('U', 'J', 'SN', '1', 'R', 'J', 'NN', '2', 'S', 'D', 'NS')
 
 for(linkR_i in 1:length(linkR_sp[['exp']])){
 	
@@ -27,4 +30,4 @@ for(linkR_i in 1:length(linkR_sp[['exp']])){
 }
 
 # Set joint degrees of freedom
-linkR_sp[['dof']] <- c('R'=1, 'P'=2, 'U'=2, 'S'=3, 'PR'=3, 'O'=3, 'L'=1)
+linkR_sp[['dof']] <- c('R'=1, 'P'=2, 'U'=2, 'S'=3, 'PR'=3, 'O'=3, 'L'=1, 'N'=6)
