4118ee47d135dd91fb287023018a3a38bf6b7057
Aaron Olsen <aaronolsen@users.noreply.github.com>
2019-05-03 15:33:35 -0400

    Added new paths

--- R/findSolvablePaths.R
+++ R/findSolvablePaths.R
@@ -1,5 +1,5 @@
 findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fixed.joints, open.joints, joint.types, 
-	body.names = NULL, indent = '\t', indent.level = 1, print.progress = FALSE){
+	body.names = NULL, joint.names = NULL, indent = '\t', indent.level = 1, print.progress = FALSE){
 
 	if(print.progress) cat(paste0(paste0(rep(indent, indent.level), collapse=''), 'findSolvablePaths()'))
 
@@ -29,20 +29,27 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 	# Vector to save which joints are open
 	is_open <- rep(TRUE, num_joints)
 
+	# By default print for all joints
+	joint_start_print <- 1:num_joints
+	#joint_start_print <- 5
+
 	# For each joint
 	for(joint_start in 1:num_joints){
 	
 		if(joint_start %in% open.joints){
-			if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+1), collapse=''), 'Skipping open joint ', joint_start))
+			if(print.progress && joint_start %in% joint_start_print) cat(paste0('\n', paste0(rep(indent, indent.level+1), collapse=''), 'Skipping open joint ', joint_start))
 			next
 		}
 
 		if(joint_start %in% input_joint_full){
-			if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+1), collapse=''), 'Skipping input joint ', joint_start))
+			if(print.progress && joint_start %in% joint_start_print) cat(paste0('\n', paste0(rep(indent, indent.level+1), collapse=''), 'Skipping input joint ', joint_start))
 			next
 		}
 
-		if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+1), collapse=''), 'Starting at joint ', joint_start))
+		if(print.progress && joint_start %in% joint_start_print){
+			cat(paste0('\n', paste0(rep(indent, indent.level+1), collapse=''), 'Starting at joint ', joint_start))
+			if(!is.null(joint.names)) cat(paste0(' (', joint.names[joint_start], ')'))
+		}
 
 		# Start lists
 		joint_path <- list(joint_start)
@@ -54,22 +61,26 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 		it <- 0
 		while(it < 100){
 
-			if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+2), collapse=''), it, ':'))
+			if(print.progress && joint_start %in% joint_start_print) cat(paste0('\n', paste0(rep(indent, indent.level+2), collapse=''), 'Iter ', it, ':'))
 
 			# Logical to test whether something has changed
 			something_changed <- FALSE
 			
-			#
+			# 
 			for(i in joint_path_idx){
 			
-				if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+3), collapse=''), i, ') '))
+				if(print.progress && joint_start %in% joint_start_print){
+					cat(paste0('\n', paste0(rep(indent, indent.level+3), collapse=''), 'Main ', i, ') '))
+				}
 
 				# Get last joint
 				last_joint <- tail(joint_path[[i]], 1)
 
 				# If NA, remove index and skip
 				if(last_joint == 0){
-					if(print.progress) cat(paste0(paste0(joint_path[[i]], collapse=','), ' (', paste0(body.names[body_path[[i]]], collapse=','), ')'))
+					if(print.progress && joint_start %in% joint_start_print){
+						cat(paste0(paste0(joint_path[[i]], collapse=','), ' (', paste0(body.names[body_path[[i]]], collapse=','), ')'))
+					}
 					#joint_path_idx <- joint_path_idx[joint_path_idx != i]
 					next
 				}
@@ -84,7 +95,9 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 				adj_joints <- adj_joints[!adj_joints %in% open.joints]
 
 				# Remove any adjacent joints that are adjacent to last two joints in path
-				# Prevent search from getting more than two joints attached the same body
+				# Prevent search from getting more than two joints attached to the same body
+				# This is just running through each add_body to do a check, a final 
+				# add_body is set later
 				if(length(adj_joints) > 0){
 
 					# Find body shared by last joint and adjacent joint
@@ -96,8 +109,24 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 						# Find body connected by both joints
 						add_body <- as.numeric(names(body_table)[body_table == 2])
 
-						# If body is already in path, mark adjacent joint as NA to skip
-						if(length(body_path) > 0 && add_body %in% body_path[[i]]) adj_joints[j] <- NA
+						if(length(body_path) > 0){
+							
+							# Stop if body is the previous two bodies in path
+							if(tail(body_path[[i]],1) == add_body && tail(body_path[[i]],2) == add_body){
+								if(print.progress && joint_start %in% joint_start_print){
+									cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'Same body is already in path twice immediately preceding new body.'))
+								}
+								adj_joints[j] <- NA
+							}
+							
+							# Stop if body is in path but not immediately preceding
+							if(tail(body_path[[i]],1) != add_body && add_body %in% body_path[[i]]){
+								if(print.progress && joint_start %in% joint_start_print){
+									cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'Same body already added earlier in path.'))
+								}
+								adj_joints[j] <- NA
+							}
+						}
 					}
 
 					# Remove any NAs
@@ -107,14 +136,17 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 				# Mark that something changed
 				something_changed <- TRUE
 
-				# Add NA to stop path
+				# If no adjacent joints, add NA to stop path
 				if(length(adj_joints) == 0){
 
 					# Add NA
 					joint_path[[i]][length(joint_path[[i]])+1] <- NA
 					joint_path_idx <- joint_path_idx[joint_path_idx != i]
 
-					if(print.progress) cat(paste0(paste0(joint_path[[i]], collapse=','), ' (', paste0(body.names[body_path[[i]]], collapse=','), ')'))
+					if(print.progress && joint_start %in% joint_start_print){
+						cat(paste0(paste0(joint_path[[i]], collapse=','), ' (', paste0(body.names[body_path[[i]]], collapse=','), ')'))
+					}
+
 					next
 				}
 
@@ -125,14 +157,14 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 				# Then create new paths with the rest
 				for(j in 1:length(adj_joints)){
 
+					# Get joint to add
+					add_joint <- adj_joints[j]
+				
 					# Get body to add
-					body_table <- table(body.conn[c(last_joint, adj_joints[j]), ])
+					body_table <- table(body.conn[c(last_joint, add_joint), ])
 
 					# Find body connected by both joints
 					add_body <- as.numeric(names(body_table)[body_table == 2])
-				
-					# Get joint to add
-					add_joint <- adj_joints[j]
 				
 					# Add 0 if fixed
 					if(add_joint %in% fixed.joints){
@@ -140,8 +172,8 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 							add_joint <- 0
 							add_body <- 1
 						}else{
-							add_joint <- c(add_joint, 0)
-							add_body <- c(add_body, 1)
+							#add_joint <- c(add_joint, 0)
+							#add_body <- c(add_body, 1)
 						}
 					}
 				
@@ -152,9 +184,11 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 						add_at <- length(joint_path)+1
 						joint_path_idx <- c(joint_path_idx, add_at)
 
-						if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+3), collapse=''), add_at, ') '))
+						if(print.progress && joint_start %in% joint_start_print){
+							cat(paste0('\n', paste0(rep(indent, indent.level+3), collapse=''), 'Spur ', add_at, ') '))
+						}
 					}
-
+					
 					# Add to lists
 					joint_path[[add_at]] <- c(joint_path_i, add_joint)
 					if(is.null(body_path_i)){
@@ -163,22 +197,31 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 						body_path[[add_at]] <- c(body_path_i, add_body)
 					}
 
-					if(print.progress) cat(paste0(paste0(joint_path[[add_at]], collapse=','), ' (', paste0(body.names[body_path[[add_at]]], collapse=','), ')'))
-					
+					if(print.progress && joint_start %in% joint_start_print){
+						#cat(paste0(paste0(joint_path[[add_at]], collapse=','), ' (', paste0(body.names[body_path[[add_at]]], collapse=','), ')'))
+						cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), paste0(joint_path[[add_at]], collapse=','), ' (', paste0(body.names[body_path[[add_at]]], collapse=','), ')'))
+					}
+
 					if(tail(add_joint, 1) == 0){
+					
+						# Check if there are additional joints
 
 						# Since reached fixed, mark path as done
 						joint_path_idx <- joint_path_idx[joint_path_idx != add_at]
 						
 						# Not adding a new joint so no need to proceed to search
 						if(length(add_joint) == 1){
-							if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'End of path, stop'))
+							if(print.progress && joint_start %in% joint_start_print){
+								cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'End of path, stop'))
+							}
 							next
 						}
 
 						# If only one joint and end, do not need to check solvable paths
 						if(length(joint_path[[add_at]]) == 2){
-							if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'Single joint to fixed path, non-solvable path'))
+							if(print.progress && joint_start %in% joint_start_print){
+								cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'Single joint to fixed path, non-solvable path'))
+							}
 							next
 						}
 					}
@@ -190,7 +233,9 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 					jt_str <- createJointPathString(joint.types=joint.types[joint_path[[add_at]]], 
 						bodies=body_path[[add_at]], mode = c('t', 'f'), ignore.joints=ignore_joints)
 
-					if(print.progress) cat(paste0(': ', jt_str[['t']], ' [', jt_str[['f']], ']'))
+					if(print.progress && joint_start %in% joint_start_print){
+						cat(paste0(': ', jt_str[['t']], ' [', jt_str[['f']], ']'))
+					}
 					
 					# Check string against solvable paths
 					if(jt_str[['t']] %in% linkR_sp[['str']][['t']]){
@@ -218,7 +263,9 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 						joint_collapse <- paste0(final_joint_path, collapse='-')
 
 						if(joint_collapse %in% added_solv_path_joints){
-							if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'Joint string matches previously found string'))
+							if(print.progress && joint_start %in% joint_start_print){
+								cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'Joint string matches previously found string'))
+							}
 							next
 						}
 
@@ -227,12 +274,16 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 						solv_path_bodies[[length(solv_path_bodies)+1]] <- final_body_path
 						added_solv_path_joints <- c(added_solv_path_joints, joint_collapse)
 
-						if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'Joint-body string matches solvable path'))
+						if(print.progress && joint_start %in% joint_start_print){
+							cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'Joint-body string matches solvable path'))
+						}
 
 					}else{
 
 						if(tail(add_joint, 1) == 0){
-							if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'No match and reached end of path, stop'))
+							if(print.progress && joint_start %in% joint_start_print){
+								cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'No match and reached end of path, stop'))
+							}
 							next
 						}
 
@@ -241,17 +292,21 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 						
 						#
 						if(any(grepl_match)){
-							if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'Joint-body string matches start of solvable path, continue'))
+							if(print.progress && joint_start %in% joint_start_print){
+								cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'Joint-body string matches start of solvable path, continue'))
+							}
 						}else{
 							joint_path_idx <- joint_path_idx[joint_path_idx != add_at]
-							if(print.progress) cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'Joint-body string does not match start of any solvable path, stop'))
+							if(print.progress && joint_start %in% joint_start_print){
+								cat(paste0('\n', paste0(rep(indent, indent.level+4), collapse=''), 'Joint-body string does not match start of any solvable path, stop'))
+							}
 						}
 					}
 				}
 			}
 			
 			if(!something_changed){
-				if(print.progress) cat(' No change')
+				if(print.progress && joint_start %in% joint_start_print) cat(' No change')
 				break
 			}
 			
@@ -262,6 +317,8 @@ findSolvablePaths <- function(joint.conn, body.conn, input.joint, input.dof, fix
 	}
 
 	if(print.progress) cat('\n')
+
+	#print(linkR_sp[['str']][['t']])
 	
 	# If no paths found, set as NULL
 	if(length(solv_path_joints) == 0) solv_path_joints <- NULL
