0b03062cae8a31f823d111e7ed3e7f84734446e1
Aaron Olsen <aaronolsen@users.noreply.github.com>
2019-05-28 19:28:41 -0400

    Added alternative method and diagnostic plotting

--- R/whichMaxShapeDist.R
+++ R/whichMaxShapeDist.R
@@ -1,4 +1,5 @@
-whichMaxShapeDist <- function(shapes, max.index = dim(shapes)[3], method = c('prod.dist'), min.per.var = 1e-3){
+whichMaxShapeDist <- function(shapes, max.index = dim(shapes)[3], method = c('disp.dist', 'prod.dist'), 
+	min.per.var = 1e-3, plot.diag = NULL){
 
 	# Make sure max.index does not exceed number of shapes
 	if(max.index > dim(shapes)[3]) max.index <- dim(shapes)[3]
@@ -23,6 +24,9 @@ whichMaxShapeDist <- function(shapes, max.index = dim(shapes)[3], method = c('pr
 	
 	# If just one column, add second highest axis so there are at least 2 columns
 	if(length(cols_use) == 1) cols_use <- order(percent.var, decreasing=TRUE)[1:2]
+	
+	# Set number of columns used
+	n_cols_use <- length(cols_use)
 	
 	# Create vector for distance index
 	dist_index <- rep(NA, max.index)
@@ -68,21 +72,70 @@ whichMaxShapeDist <- function(shapes, max.index = dim(shapes)[3], method = c('pr
 
 			n <- n + 1
 		}
+		
+		max_dist_pts <- NULL
 
-		if(FALSE){
-			print(dist_index)
-			print(scores[dist_index, 1:3])
-			cols <- rainbow(n=length(dist_index), start=0, end=5/6)
-
-			# Find center of score space
-			center <- colMeans(scores[1:2, cols_use])
+	}else if(method[1] == 'disp.dist'){
 	
-			svg.new(file='scores.html')
-			svg.points(scores[, 1:3], opacity.stroke=0.05, opacity.fill=0.05)
-			svg.points(scores[dist_index, 1:3], cex=c(1:max.index)+1, col.stroke=cols, col.fill='none')
-			svg.frame(scores[, 1:3])
-			svg.close()
+		# Create point set matrix for maximally distributed across all axes
+		max_dist_pts <- matrix(NA, nrow=max.index, ncol=n_cols_use)
+
+		# Get number of rows
+		nrows <- sqrt(max.index)
+		
+		# Check that max index is a 2-power number
+		if(nrows - floor(nrows) != 0) stop('If method is "disp.dist" then "max.index" must be a number to the power of 2 (e.g. 4, 9, 16, 25, ...).')
+
+		# Fill matrix
+		for(i in 1:n_cols_use){
+
+			# Get total range
+			score_range <- range(scores[, i])
+
+			# Get values in "grid" pattern
+			if(i %% 2 == 0){
+				max_dist_pts[,i] <- seq(score_range[1], score_range[2], length=nrows)
+			}else{
+				max_dist_pts[,i] <- sort(rep(seq(score_range[1], score_range[2], length=nrows), nrows))
+			}
 		}
+		
+		# Find scores closest to grid points
+		for(i in 1:max.index){
+
+			# Find distance from grid point to all points
+			dists <- distPointToPoint(max_dist_pts[i,], scores_mat[, cols_use])
+			
+			# Take point that is closest
+			dist_index[i] <- which.min(dists)
+
+			# Set to NA so not chosen again
+			scores_mat[dist_index[i], ] <- NA
+		}
+	}
+
+	# Plot
+	if(!is.null(plot.diag)){
+
+		# Set number of plot rows
+		n_plot_cols <- 3
+		n_plot_rows <- ceiling((n_cols_use-1) / n_plot_cols)
+		
+		if(n_cols_use < 3) n_plot_cols <- n_cols_use
+		
+		pdf(plot.diag, width=n_plot_cols*3, height=n_plot_rows*2.8)
+
+		layout(mat=matrix(1:(n_plot_cols*n_plot_rows), n_plot_rows, n_plot_cols, byrow=TRUE))
+		par(mar=c(5,5,2,2))
+		
+		for(i in 1:(n_cols_use-1)){
+			plot(scores[, i:(i+1)], xlab=paste0('PC', i, ' (', round(percent.var[i], 2), '%)'), ylab=paste0('PC', i+1, ' (', round(percent.var[i+1], 2), '%)'), col=rgb(0,0,0,0.1))
+			points(scores[dist_index, i:(i+1)], col='red', cex=0.8)
+			points(scores[dist_index, i:(i+1)], type='l', col=rgb(1,0,0,0.3))
+			if(!is.null(max_dist_pts)) points(max_dist_pts[, i:(i+1)], col=rgb(0,1,0,0.2), cex=0.8)
+		}
+
+		dev.off()
 	}
 	
 	dist_index
